59ff69c3cc14127e046562558a9642cdf2cb5692
dgj7 <garciad48879@gmail.com>
2024-10-17 21:29:27 -0400

    transcoder|java: add simple/empty spring-based web project

---
+++ rtsp-to-mp4-transcoder/src/main/java/com/dg/securitycams/transcoder/RtspToMp4TranscoderApplication.java
@@ -0,0 +1,13 @@
+package com.dg.securitycams.transcoder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RtspToMp4TranscoderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RtspToMp4TranscoderApplication.class, args);
+	}
+
+}
---
+++ rtsp-to-mp4-transcoder/src/test/java/com/dg/securitycams/transcoder/RtspToMp4TranscoderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dg.securitycams.transcoder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RtspToMp4TranscoderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
